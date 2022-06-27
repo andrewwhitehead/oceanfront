2e0e94c823796ff274a65cfeb9a98a65b7db11a2
roman21 <r.gidion@unifiedcommerce.cloud>
2022-06-27 17:26:18 +0700

    3. Oceanfront - Limit the height of dialog box, add more examples of different dialogs for testing https://github.com/1CRM/oceanfront/issues/143

--- packages/demo/src/components/Popups.vue
+++ packages/demo/src/components/Popups.vue
@@ -8,7 +8,23 @@
       <template #header></template>
       <template #default>
         <div class="container">
-          <h4>Test Dialog</h4>
+          <h4>Simple Dialog</h4>
+          <of-select-field :items="['one', 'two']" label="Test select" />
+        </div>
+      </template>
+      <template #footer></template>
+    </of-dialog>
+
+    <p></p>
+
+    <of-button @click="toggleSecondDialog">
+      Show dialog (with nested dialog)
+    </of-button>
+    <of-dialog v-model="secondDialogActive">
+      <template #header></template>
+      <template #default>
+        <div class="container">
+          <h4>Dialog with nested</h4>
           <of-select-field :items="['one', 'two']" label="Test select" />
           <p>
             <of-button @click="toggleNestedDialog">
@@ -22,6 +38,28 @@
             <of-select-field :items="['one', 'two']" label="Test select" />
           </div>
         </of-dialog>
+      </template>
+      <template #footer></template>
+    </of-dialog>
+
+    <p></p>
+
+    <of-button @click="toggleThirdDialog"> Show dialog with data </of-button>
+    <of-dialog v-model="thirdDialogActive">
+      <template #header></template>
+      <template #default>
+        <div class="container">
+          <h4>Dialog with data</h4>
+          <p>
+            Instead of binding each field's value property directly, it can be
+            more effective to bind a <var>record</var> and a field name. This
+            may be be may be be required for complex field types which bind to
+            multiple fields on the associated record. It also allows for
+            validation rules to be combined at the record level, instead of
+            being assigned to individual fields or handled within a custom
+            component.
+          </p>
+        </div>
       </template>
       <template #footer></template>
     </of-dialog>
@@ -39,14 +77,25 @@ export default defineComponent({
 />
 `
     const dialogActive = ref(false)
+    const secondDialogActive = ref(false)
+    const thirdDialogActive = ref(false)
+
     const nestedDialogActive = ref(false)
 
     return {
       dialogActive,
+      secondDialogActive,
+      thirdDialogActive,
       nestedDialogActive,
       sampleCode,
       toggleDialog: () => {
         dialogActive.value = !dialogActive.value
+      },
+      toggleSecondDialog: () => {
+        secondDialogActive.value = !secondDialogActive.value
+      },
+      toggleThirdDialog: () => {
+        thirdDialogActive.value = !thirdDialogActive.value
       },
       toggleNestedDialog: () => {
         nestedDialogActive.value = !nestedDialogActive.value
