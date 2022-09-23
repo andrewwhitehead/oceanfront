013f38d9e4a2e8067e47b5e8901b6b5b78cbb793
ivan-b <ivan.bibik@gmail.com>
2022-09-23 16:45:56 +0300

    Value Formatters page refacror, moved Url Input

--- packages/demo/src/components/Formatters.vue
+++ packages/demo/src/components/Formatters.vue
@@ -1,62 +1,54 @@
 <template>
   <div class="container">
     <h1>Value Formatters</h1>
-
-    <div class="row">
-      <div class="column sm-6">
-        <h4>Number Input</h4>
-        <of-config :locale="locale">
-          <of-field
-            style="width: 10em"
-            format="number"
-            v-model="values.number"
-          />
-          <br />
-          Output: <of-format type="number" :value="values.number" />
-        </of-config>
+    <div class="row content">
+      <div class="column">
+        <div class="demo-form of--elevated-1">
+          <div class="row form-row" style="column-gap: 1em">
+            <div class="column">
+              <of-config :locale="locale">
+                <of-text-field
+                  placeholder="(placeholder)"
+                  help="field help"
+                  label="Number Input"
+                  v-model="values.number"
+                  type="number"
+                />
+              </of-config>
+            </div>
+            <div class="column">
+              <of-select-field
+                label="Select Locale"
+                :items="localeOpts"
+                v-model="locale"
+              />
+            </div>
+          </div>
+          <div class="row form-row">
+            <of-config :locale="locale">
+              <of-field
+                placeholder="(placeholder)"
+                help="field help"
+                label="Duration Input"
+                type="duration"
+                v-model="values.duration"
+              />
+            </of-config>
+          </div>
+          <div class="row form-row">
+            <of-config :locale="locale">
+              <of-field
+                placeholder="(placeholder)"
+                help="field help"
+                label="URL Input"
+                type="url"
+                v-model="values.url"
+              />
+            </of-config>
+          </div>
+        </div>
       </div>
-      <div class="column sm-6">
-        <h4>Select Locale</h4>
-        <of-select-field :items="localeOpts" v-model="locale" />
-        <br />
-        <of-config :locale="locale">
-          <template #default="{ loc }"> Effective locale: {{ loc }} </template>
-        </of-config>
-      </div>
-    </div>
-    <div class="row">
-      <div class="column sm-6">
-        <h4>Duration Input</h4>
-        <of-config :locale="locale">
-          <of-field
-            style="width: 10em"
-            format="duration"
-            v-model="values.duration"
-          />
-          <br />
-          Output:
-          <of-format type="duration" :value="values.duration" />
-        </of-config>
-      </div>
-    </div>
-    <div class="row">
-      <div class="column sm-6">
-        <h4>URL Input</h4>
-        <of-field style="width: 10em" format="url" v-model="values.url" />
-      </div>
-      <div class="column sm-4"></div>
-    </div>
-    <div class="row">
-      <div class="column sm-6">
-        <h4>Color Input</h4>
-        <of-field
-          style="width: 10em"
-          type="text"
-          format="color"
-          v-model="values.color"
-        />
-      </div>
-      <div class="column sm-4"></div>
+      <div class="column"></div>
     </div>
   </div>
 </template>
